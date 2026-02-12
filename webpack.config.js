39e7a08ec935a8a870215f8100f2845481921103
Shengzhe Zhang <shzhang0907@gmail.com>
2026-02-12 00:10:54 -0800

    update api

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
@@ -55,6 +56,7 @@ module.exports = {
     usedExports: true, // Removes unused functions
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html", // Generates dist/index.html
